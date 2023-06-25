9cf6600516551d24d216a727f9708053eb75c269
Kevin <kevhho@my.yorku.ca>
2023-06-24 21:45:59 -0400

    Completed first iteration

--- script.js
+++ script.js
@@ -1,28 +1,70 @@
 const rock = "1";
 const paper = "2";
 const scissor = "3";
-let comNum, comAns;
-let playerSelection = prompt("Please enter choice:");
+let playerTally = 0;
+let compTally = 0;
 
 const getComputerChoice = () => {
   switch ((comNum = Math.floor(Math.random() * 3 + 1))) {
     case 1:
-      comAns = "Rock";
-      return comAns;
+      return "rock";
     case 2:
-      comAns = "Paper";
-      return comAns;
+      return "paper";
     case 3:
-      comAns = "Scissor";
-      return comAns;
+      return "scissors";
   }
 };
 
 function playRound(playerSelection, computerSelection) {
-    for (let i = 0; i < 6; i++) {
-        playerSelection.toLowerCase();
-        console.log(i);
-    }
+  playerSelection = playerSelection.toLowerCase();
+  switch (playerSelection) {
+    case "rock":
+      if (computerSelection == "paper") {
+        compTally++;
+        return "You Lose! Paper Beats Rock!";
+      } else if (computerSelection == "scissors") {
+        playerTally++;
+        return "You Win! Rock Beats Scissors!";
+      } else {
+        return "DRAW";
+      }
+    case "paper":
+      if (computerSelection == "scissors") {
+        compTally++;
+        return "You Lose! Scissors Beats Rock!";
+      } else if (computerSelection == "rock") {
+        paperTally++;
+        return "You Win! Paper Beats Rock!";
+      } else {
+        return "DRAW";
+      }
+    default: // Assuming input is sterilized I guess
+      if (computerSelection == "rock") {
+        compTally++;
+        return "You Lose! Rock Beats Scissors!";
+      } else if (computerSelection == "paper") {
+        paperTally++;
+        return "You Win! Scissors Beats Paper!";
+      } else {
+        return "DRAW";
+      }
+  }
 }
-const computerSelection = getComputerChoice();
-playRound(playerSelection, computerSelection);
+
+function game() {
+  playerTally = 0;
+  compTally = 0;
+  for (let i = 0; i < 5; i++) {
+    const computerSelection = getComputerChoice();
+    console.log(
+      playRound(
+        (playerSelection = prompt("Please enter choice:")),
+        computerSelection
+      )
+    );
+    // console.log(`PLA: ${playerSelection}, COM: ${computerSelection}`) debug line
+  }
+  console.log(`PLA: ${playerTally} - COM: ${compTally}`)
+}
+
+game();
